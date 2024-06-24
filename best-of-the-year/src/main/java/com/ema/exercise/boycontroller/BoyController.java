ad9975c175c7e29aa800ea7e2c3010328447eeaf
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-06-24 20:38:45 +0200

    Funziona!

--- best-of-the-year/src/main/java/com/ema/exercise/boycontroller/BoyController.java
+++ best-of-the-year/src/main/java/com/ema/exercise/boycontroller/BoyController.java
@@ -1,18 +1,18 @@
-package boy.controller;
+package com.ema.exercise.boycontroller;
 
 import java.time.LocalDateTime;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BoyController {
 	
-	@GetMapping("/saluta")
-	public String saluta(@RequestParam(name = "nome", required = false) String nome, Model model) {
-		model.addAttribute("name", nome);
+	@GetMapping("/boy")
+	public String boy (@RequestParam(name = "name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
 		model.addAttribute("time", LocalDateTime.now());
 		return "boy";
 	}
