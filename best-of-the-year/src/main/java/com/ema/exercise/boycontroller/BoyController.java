d584d30d830493bc9410643c30eb767bad891dc8
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-05 23:14:12 +0200

    fine esercitazione
    aggiunto collegamento a bootstrap, creato una card con i dati, modificato campo dati che precede "name", separato dateTime in due variabili differenti e stampante con thymeleaf dalla view

--- best-of-the-year/src/main/java/com/ema/exercise/boycontroller/BoyController.java
+++ best-of-the-year/src/main/java/com/ema/exercise/boycontroller/BoyController.java
@@ -1,7 +1,11 @@
 package com.ema.exercise.boycontroller;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +13,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoyController {
-	
+
 	@GetMapping("/boy")
-	public String boy (@RequestParam(name = "name", required = false) String name, Model model) {
+	public String boy(@RequestParam(name = "name", required = false) String name, Model model) {
+
 		model.addAttribute("name", name);
-		model.addAttribute("time", LocalDateTime.now());
+		model.addAttribute("date", LocalDate.now());
+
+		// metodo per formattare l'ora
+		String formattedTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+		// Aggiungo l'ora formattata al modello
+		model.addAttribute("time", formattedTime);
+
 		return "boy";
 	}
+
 }
